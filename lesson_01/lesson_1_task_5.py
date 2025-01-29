e377bf5b0c8ad1af05abd111d091fd529d3c2751
AlexQASkypro <alexqaskypro@gmail.com>
2025-01-29 15:21:33 +0300

    Delete lesson_01 directory

--- lesson_01/lesson_1_task_5.py
+++
@@ -1,9 +0,0 @@
-def print_number(num):  
-    print(num)  
-  
-# Номер, который нужно напечатать  
-phone_number = "88005553535"  
-  
-# Вызываем функцию 11 раз  
-for _ in range(11):  
-    print_number(phone_number)  
