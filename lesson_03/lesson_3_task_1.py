3b8432292689720ddaf45422ededfebe4dac444d
AlexQASkypro <alexqaskypro@gmail.com>
2025-02-02 20:01:20 +0300

    Delete lesson_03 directory

--- lesson_03/lesson_3_task_1.py
+++
@@ -1,9 +0,0 @@
-from user import User  
-  
-# Создаем новый экземпляр User  
-my_user = User("Иван", "Иванов")  
-  
-# Вызываем методы у объекта my_user  
-my_user.print_first_name()  # Печатает имя  
-my_user.print_last_name()   # Печатает фамилию  
-my_user.print_full_name()   # Печатает имя и фамилию  
