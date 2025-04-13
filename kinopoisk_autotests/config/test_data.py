c655e02d6135b65fb3d9d324c55801c55fd91299
AlexQASkypro <alexqaskypro@gmail.com>
2025-04-13 18:39:50 +0300

    Delete kinopoisk_autotests directory

--- kinopoisk_autotests/config/test_data.py
+++
@@ -1,9 +0,0 @@
-class TestData:
-    # Данные для API тестов
-    STUDIO_SEARCH_PARAMS = {"page": 1, "limit": 10, "selectFields": "id"}
-    KEYWORD_SEARCH_PARAMS = {"page": 1, "limit": 10, "title": "Бунтарский дух"}
-    IMAGE_SEARCH_PARAMS = {"page": 1, "limit": 10, "movieId": 666}
-
-    # Данные для UI тестов
-    TEST_MOVIE_NAME = "Матрица"
-    TEST_PROMO_CODE = "TEST2023"
